6576dc976d07b07d9f5f185579053d39f1d46413
Ubuntu <ubuntu@ip-10-0-75-112.eu-west-1.compute.internal>
2023-11-01 23:56:35 +0000

    create vpc

--- infrastructure/terraform_aws/VPC/vpc.tf
+++ infrastructure/terraform_aws/VPC/vpc.tf
@@ -1,27 +1,135 @@
-resource "aws_vpc" "terraform-vpc" {
-  cidr_block       = "172.31.0.0/16"
-  instance_tenancy = "default"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  # Must be enabled for EFS
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
-    Name = "terraform-vpc"
+    Name = "main"
   }
 }
 
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.terraform-vpc.id
-  cidr_block        = "172.31.0.0/16"
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "tf-example"
+    Name = "igw"
   }
 }
 
-resource "aws_network_interface" "foo" {
-  subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.31.7.90"]
+resource "aws_subnet" "private-eu-west-1a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/19"
+  availability_zone = "eu-west-1a"
 
   tags = {
-    Name = "primary_network_interface"
+    "Name"                                      = "private-eu-west-1a"
+    "kubernetes.io/role/internal-elb"           = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
   }
+}
+
+resource "aws_subnet" "private-eu-west-1b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.32.0/19"
+  availability_zone = "eu-west-1b"
+
+  tags = {
+    "Name"                                      = "private-eu-west-1b"
+    "kubernetes.io/role/internal-elb"           = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "public-eu-west-1a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.64.0/19"
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                                      = "public-eu-west-1a"
+    "kubernetes.io/role/elb"                    = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "public-eu-west-1b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.96.0/19"
+  availability_zone       = "eu-west-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                                      = "public-eu-west-1b"
+    "kubernetes.io/role/elb"                    = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+  }
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+
+  tags = {
+    Name = "nat"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public-eu-west-1a.id
+
+  tags = {
+    Name = "nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "private"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table_association" "private-eu-west-1a" {
+  subnet_id      = aws_subnet.private-eu-west-1a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private-eu-west-1b" {
+  subnet_id      = aws_subnet.private-eu-west-1b.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "public-eu-west-1a" {
+  subnet_id      = aws_subnet.public-eu-west-1a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-eu-west-1b" {
+  subnet_id      = aws_subnet.public-eu-west-1b.id
+  route_table_id = aws_route_table.public.id
 }
 
