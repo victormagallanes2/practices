b18e68e63b4fdeaebbaa1adc9c8dc785289b5e59
Ubuntu <ubuntu@ip-10-0-75-112.eu-west-1.compute.internal>
2023-11-23 02:35:14 +0000

    update terraform ec2

--- infrastructure/terraform_aws/EC2/ec2.tf
+++ infrastructure/terraform_aws/EC2/ec2.tf
@@ -1,13 +1,12 @@
 resource "aws_instance" "terraform-ec2" {
-  ami           = "ami-0097e4945b2d15c30"
+  ami           = "ami-0ad8410c434dca64c"
   instance_type = "t2.micro"
-  key_name = "my-key-terraform-ec2"
   vpc_security_group_ids  = [var.ssh-access]
-  subnet_id = [var.public_subnet_id_a,var.public_subnet_id_b]
+  subnet_id = var.public_subnet_id_a
 
 
   tags = {
-    Name = "ubuntu20.04"
+    Name = "ubuntu22.04"
   }
 }
 
