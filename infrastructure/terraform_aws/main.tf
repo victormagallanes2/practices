17360d0dc24649a93fb62c10682f511ee5fbf2d4
victor <victormagallanes2@gmail.com>
2023-11-22 21:56:12 -0400

    add ec2

--- infrastructure/terraform_aws/main.tf
+++ infrastructure/terraform_aws/main.tf
@@ -49,4 +49,14 @@ module "RDS" {
   rds_company = module.SecurityGroup.rds_company
 }
 
+module "EC2" {
+  source = "./EC2"
+  public_subnet_id_a = module.VPC.public-eu-west-1a-id
+  public_subnet_id_b = module.VPC.public-eu-west-1b-id
+  private_subnet_id_a = module.VPC.private-eu-west-1a-id
+  private_subnet_id_b = module.VPC.private-eu-west-1b-id
+  vpc_id = module.VPC.vpc_id
+  ssh-access = module.SecurityGroup.ssh-access
+}
+
 
