21cd8c96ac12b274b52e650c16f35c852c133516
Ubuntu <ubuntu@ip-10-0-75-112.eu-west-1.compute.internal>
2023-11-28 01:38:39 +0000

    update s3 module

--- infrastructure/terraform_aws/S3/s3.tf
+++ infrastructure/terraform_aws/S3/s3.tf
@@ -1,43 +1,23 @@
 #Resource to create s3 bucket
-resource "aws_s3_bucket" "demo-bucket"{
-  bucket = "ck-demo-bucket-18th"
+resource "aws_s3_bucket" "buckettest7770"{
+  bucket = "buckettest7770"
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
 
   tags = {
-    Name = "S3Bucket"
-  }
-}
-
-#Resource to enable versioning 
-resource "aws_s3_bucket_versioning" "versioning_demo" {
-  bucket = aws_s3_bucket.demo-bucket.id
-  versioning_configuration {
-    status = "Enabled"
+    Name = "buckettest7770"
   }
 }
 
 #Resource to enable encryption
-resource "aws_s3_bucket_server_side_encryption_configuration" "demo-encryption" {
-  bucket = aws_s3_bucket.demo-bucket.bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "buckettest7770-demo" {
+  bucket = aws_s3_bucket.buckettest7770.bucket
 
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
   }
-}
-
-#Adds an ACL to bucket
-resource "aws_s3_bucket_acl" "demo_bucket_acl" {
-  bucket = aws_s3_bucket.demo-bucket.bucket
-  acl    = "private"
-}
-
-#Block Public Access
-resource "aws_s3_bucket_public_access_block" "demo_public_block" {
-  bucket = aws_s3_bucket.demo-bucket.bucket
-
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls = true
-  restrict_public_buckets = true
 }
